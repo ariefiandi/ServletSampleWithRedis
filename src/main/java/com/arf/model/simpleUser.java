27113bf6a7d1a2e3e0115f46085665c609fd0163
Ariefiandi Nugraha <ariefiandi.nugraha@elephanttalk.com>
2016-07-08 12:50:32 +0700

    initiate repo : - use maven as package repository - create basic simpeUser model - create redis repository

---
+++ src/main/java/com/arf/model/simpleUser.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arf.model;
+
+/**
+ *
+ * @author AriefiandiN
+ */
+public class simpleUser {
+    private String name;
+    private String password;
+    private String email;
+    private String country;
+
+    public simpleUser() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    
+}
